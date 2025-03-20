9bc45df3c531594677f9bed494870d423c294ce5
Kryssz <kryssz77@gmail.com>
2025-03-20 14:51:42 +0100

    Comments

--- Selenium/Trial_of_the_Stone/trial_of_the_stones.py
+++ Selenium/Trial_of_the_Stone/trial_of_the_stones.py
@@ -18,19 +18,23 @@ class TestTC(object):
         self.driver.quit()
 
     def test_trial(self):
+        #Solving the first riddle, with the "rock" string.
         stone = self.driver.find_element(By.XPATH, '//input[@id="r1Input"]')
         stone.send_keys('rock')
         answer1 = self.driver.find_element(By.XPATH, '//button[@id="r1Btn"]')
         answer1.click()
 
+        #Solving the second riddle, use the revealed password on the site.
         secrets = self.driver.find_element(By.XPATH, '//input[@id="r2Input"]')
         key1 = self.driver.find_element(By.XPATH, '//div[@id="passwordBanner"]/h4')
         secrets.send_keys(key1.text)
         answer2 = self.driver.find_element(By.XPATH, '//button[@id="r2Butn"]')
         answer2.click()
+        #Assert to solve the first and the second riddle.
         success1 = self.driver.find_element(By.ID, 'successBanner1')
         assert success1.text == 'Success!'
 
+        #Compare the 2 merchant wealth, and use the name of the wealthiest merchant in the text field.
         merchant_blocks = self.driver.find_elements(By.XPATH, '//div[span/b]')
         merchant_data = []
         for block in merchant_blocks:
@@ -44,10 +48,12 @@ class TestTC(object):
         riddle_input = self.driver.find_element(By.ID, 'r3Input')
         riddle_input.send_keys(name)
 
+        #Assert to solv the third riddle.
         self.driver.find_element(By.ID, 'r3Butn').click()
         success2 = self.driver.find_element(By.ID, 'successBanner2')
         assert success2.is_displayed()
 
+        #Assert to solve all of the riddles.
         self.driver.find_element(By.ID, 'checkButn').click()
         success_banner = self.driver.find_element(By.ID, 'trialCompleteBanner')
         assert success_banner.is_displayed()
