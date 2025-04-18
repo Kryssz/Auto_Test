a0f552e3ce6573388bcdc4502e8cf3d6622cb05f
Kryssz <kryssz77@gmail.com>
2025-04-18 12:00:09 +0200

    Form validation test.

---
+++ Selenium/Form validation/TESTDATA.py
@@ -0,0 +1,11 @@
+TESTDATA = {
+'E-Mail': 'yardy@yarr.com',
+'Password': '123456',
+'Confirm': '123456',
+'Customer_number': '7',
+'Dealer_number': '1234',
+'Date': '2025-04-12',
+'Random_text': 'text',
+'Card_number': '4111111111111111',
+'CVV': '123'
+}
