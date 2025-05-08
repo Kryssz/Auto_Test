2754943b5a8f468d98996dac8d75989167d6166b
Kryssz <kryssz77@gmail.com>
2025-05-08 12:03:27 +0200

    Selenium practice.

---
+++ Selenium/Perimeter test/TESTDATA.py
@@ -0,0 +1,17 @@
+TESTDATA_RECTANGLE_HAPPY = {
+    'a': '74',
+    'b': '32',
+    'result': '212'
+}
+
+TESTDATA_RECTANGLE_UNHAPPY_LETTER = {
+    'a': 'kiskutya',
+    'b': '32',
+    'result': 'NaN'
+}
+
+TESTDATA_RECTANGLE_UNHAPPY_EMPTY = {
+    'a': '',
+    'b': '',
+    'result': 'NaN'
+}
